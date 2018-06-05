2007525b785b28f0dcaf82032f8e84589a8b3fcd
Gaurav Verma <gaverma7@in.ibm.com>
2018-06-05 12:33:49 +0530

    changed todo import in server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 var port = process.env.PORT || 3001;
